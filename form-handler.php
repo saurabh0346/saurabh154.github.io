bd0f9831dabb003a04092f3217df30ccb51171ce
saurabh0346 <111052176+saurabh0346@users.noreply.github.com>
2022-08-11 16:09:32 +0530

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'https://saurabh0346.github.io/saurabh154.github.io/'
+$email_from = 'saurabh154.github.io'
 
 $email_subject = 'New Form Submission'
 
